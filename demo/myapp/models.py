1e3ee4c76e675cdcc3bd04610ca0427702767120
Alan Zhainar <alanzhainar05@mail.ru>
2024-06-26 02:22:15 +0500

    added initial authentication

--- demo/myapp/models.py
+++ demo/myapp/models.py
@@ -1,6 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class TodoItem(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='todo_items')
     title = models.CharField(max_length=200)
     reps = models.FloatField(default=0.0)
     completed = models.BooleanField(default=False)
--- demo/myapp/urls.py
+++ demo/myapp/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
@@ -9,4 +10,7 @@ urlpatterns = [
     path("edit/<int:pk>/", views.edit_todo, name="edit_todo"),
     path("delete/<int:pk>/", views.delete_todo, name="delete_todo"),
     path("workouts/progress-graph/<int:pk>/", views.progress_graph, name="progress_graph"),
+    path("login/", auth_views.LoginView.as_view(template_name='login.html'), name="login"),
+    path("logout/", auth_views.LogoutView.as_view(next_page='home'), name="logout"),
+    path("register/", views.register, name="register"),
 ]
