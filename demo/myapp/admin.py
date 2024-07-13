6f1ded86f8715fd58bfd05671aeb1459cb91c9e2
Alan Zhainar <alanzhainar05@mail.ru>
2024-07-13 16:23:27 +0500

    food entries change, structure change

--- demo/myapp/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from .models import WorkoutItem
-
-# Register your models here.
-admin.site.register(WorkoutItem)
--- demo/myapp/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import FoodEntry
-
-class FoodEntrySerializer(serializers.ModelSerializer):
-    class Meta:
-        model = FoodEntry
-        fields = ['id', 'name', 'calories', 'carbs', 'protein', 'fats', 'date']
